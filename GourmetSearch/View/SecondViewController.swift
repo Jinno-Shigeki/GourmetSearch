55b1417fc1cc7b208d5b46cb927fc70e7bf35822
jinn <booboo080yamaya@gmail.com>
2020-06-06 17:15:10 +0900

    tabBarController追加

---
+++ GourmetSearch/View/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  GourmetSearch
+//
+//  Created by 神野成紀 on 2020/06/06.
+//  Copyright © 2020 神野成紀. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
