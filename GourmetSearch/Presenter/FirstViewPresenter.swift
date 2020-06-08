60952d7fc30c585aa05ade5990a3d94347f5415f
jinn <booboo080yamaya@gmail.com>
2020-06-09 05:36:30 +0900

    api通信の追加

--- GourmetSearch/Presenter/FirstViewPresenter.swift
+++ GourmetSearch/Presenter/FirstViewPresenter.swift
@@ -7,8 +7,35 @@
 //
 
 import Foundation
+import Alamofire
 
 class FirstViewPresenter {
     let category = ["和食","洋食","イタリアン","焼肉","寿司","ラーメン"]
     let shopSystem = ["飲み放題","食べ放題","ランチ","個室","デート"]
+    var gourmetData: [GourmetData]  = []
+    
+    func getGourmetData(word: String) {
+        let urlString = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
+        let parameters: [String: Any] = ["keyid": "2ab0f587c42b6257ca957161c69732c9","freeword": word, "hit_per_page": 20]
+        Alamofire.request(urlString, parameters: parameters).responseJSON {[weak self] (response) in
+            guard let self = self else {return}
+            if let data = response.data {
+                self.parseJson(apiData: data)
+            } else if let err = response.error {
+                print("error")
+            }
+        }
+    }
+    
+    func parseJson(apiData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(Entity.self, from: apiData)
+            decodedData.rest.forEach { (rest) in
+                gourmetData.append(GourmetData(rest: rest))
+            }
+        } catch {
+            print("error!")
+        }
+    }
 }
